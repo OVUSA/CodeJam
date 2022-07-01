76cbf58202478a26fd879ed66ae302e889359d32
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2022-07-01 17:53:06 -0500

    Double or One Thing
    https://codingcompetitions.withgoogle.com/codejam/round/0000000000877ba5/0000000000aa8e9c

---
+++ Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution{
+
+    public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    int test = sc.nextInt();
+    
+    for( int i = 0;i<test;i++){
+        String one =sc.next();
+        System.out.println(one);
+        
+    }
+    
+    
+    }
+}
